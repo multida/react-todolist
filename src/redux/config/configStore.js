83e4dd843c355065897b0e2fc8be6cad50c85c60
multida <wow_da65@naver.com>
2023-07-16 21:10:57 +0900

    🚧 리덕스 구조 세팅

---
+++ src/redux/config/configStore.js
@@ -0,0 +1,9 @@
+//중앙 데이터 관리소(state)를 설정하는 부분
+
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({});
+const store = createStore(rootReducer);
+
+export default store;
